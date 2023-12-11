8de4fc2159dc4b45571fb7dbb8a7b5140b55562a
Steven Kalt <kalt.steven@gmail.com>
2023-12-11 15:22:38 -0500

    fix: use oxalica/rust-overlay properly

--- flake.nix
+++ flake.nix
@@ -1,35 +1,38 @@
 {
-  description = "TODO: add a description";
+  description = ""; # FIXME: add a description
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils"; # TODO: pin
+    rust-overlay.url = "github:oxalica/rust-overlay"; # TODO: pin
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, flake-utils, nixpkgs }:
+  outputs = { self, flake-utils, nixpkgs, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        overlays = [ (import rust-overlay) ];
         pkgs = (import nixpkgs) {
-          inherit system;
+          inherit system overlays;
         };
         # Generate a user-friendly version number.
         version = builtins.substring 0 8 self.lastModifiedDate;
-        cargoDeps = pkgs.rustPlatform.importCargoLock {
-          lockFile = ./Cargo.lock;
-        };
+        rust_toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+        info = (builtins.fromTOML (builtins.readFile ./Cargo.toml));
       in
-      rec {
+      {
         packages = {
           # For `nix build` & `nix run`:
           default = pkgs.rustPlatform.buildRustPackage {
             # https://nixos.org/manual/nixpkgs/stable/#compiling-rust-applications-with-cargo
             inherit version;
-            pname = "pg_walk";
-            src = ./.;
-            nativeBuildInputs = with pkgs; [
-              cargo
-              clippy
-              rustc
-            ];
+            pname = info.package.name;
+            src = ./.; # TODO: narrow
+              cargoLock = {
+                lockFile = ./Cargo.lock;
+              };
+            # see https://johns.codes/blog/efficient-nix-derivations-with-file-sets
+            # see https://github.com/JRMurr/roc2nix/blob/main/lib/languageFilters.nix
+            nativeBuildInputs = [ rust_toolchain ];
           };
         };
         # For `nix develop`:
@@ -38,21 +41,27 @@
           # see https://hoverbear.org/blog/rust-bindgen-in-nix/
           # see https://slightknack.dev/blog/nix-os-bindgen/
           # https://nixos.wiki/wiki/Rust#Installation_via_rustup
-          nativeBuildInputs = with pkgs; [
-            cargo
-            clippy
-            rustc
-          ];
+          nativeBuildInputs = [ rust_toolchain ];
           buildInputs = with pkgs;
             [
-              cargo-bloat
-              nixpkgs-fmt
-              pkgconfig
-              rnix-lsp
-              rust-analyzer
+              # rust tools
+              rust-analyzer-unwrapped
               rustfmt
-              helix
+              cargo-bloat
+
+              # nix support
+              nixpkgs-fmt
+              nil
+
+              # other
+              lychee
+              shellcheck
+              git
+              bashInteractive
             ];
+
+          # Environment variables
+          RUST_SRC_PATH = "${rust_toolchain}/lib/rustlib/src/rust/library";
         };
       }
     );
